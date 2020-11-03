d673868886d7c6961273858443da1df1b784cf0a
MuizMustafa <mmust103@uottawa.ca>
2020-11-03 00:30:39 -0500

    Tutorial video and safety scenes worked on
    Tutorial Video scenes are fully functional as well as safety warning scenes. except for a volume controller on the video and an invisible next button on the video screen.

---
+++ AR Construction/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+   public void TutorialVideo1()
+    {
+        SceneManager.LoadScene(1);
+    }
+   public void TutorialSelect()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void OnSiteOffSite()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void OnSiteWarning()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void OffSiteWarning()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
